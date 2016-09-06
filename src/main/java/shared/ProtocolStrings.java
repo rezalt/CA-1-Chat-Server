e48ab2a966b3c4220d220c092138a5ebde2a6092
Joakim <Joakim@10.50.138.89>
2016-09-06 12:43:44 +0200

    getMethod og tilh. metoder start

--- src/main/java/shared/ProtocolStrings.java
+++ src/main/java/shared/ProtocolStrings.java
@@ -6,6 +6,40 @@ public class ProtocolStrings {
         STOP,LOGIN,MSG,LOGOUT,CLIENTLIST,MSGRESP;
     }
     
+    
+    public void getMethod(String q){
+        if(q.startsWith("MSG:")){
+            message(q);
+        }
+        if(q.startsWith("LOGIN:")){
+            
+        }
+        if(q.startsWith("LOGOUT:")){
+            
+        }
+        if(q.startsWith("MSGRES:")){
+            
+        }
+        if(q.startsWith("CLIENTLIST:")){
+            
+        }
+    }
+    
+    public String message(String q){
+        return q.substring(4);
+    }
+    public String login(){
+        return "Login Success";
+    }
+    public String logout(){
+        return "Logged out!";
+    }
+    public String clientList(){
+        return "";
+    }
+    public String msgRes(){
+        return "";
+    }
     public String sendMessage(ARGS args, String msg){
         if(args.equals(ARGS.MSGRESP)){
             
