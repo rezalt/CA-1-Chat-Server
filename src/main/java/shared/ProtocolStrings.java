acdb76fe19c21a836350d1a7f81074083a1f2ec9
TimmosQuadros <timm.rasmussen@gmail.com>
2016-09-06 11:46:59 +0200

    Tilføjede shared package og en ProtocolStrings klasse til at lave skabeloner.

---
+++ src/main/java/shared/ProtocolStrings.java
@@ -0,0 +1,11 @@
+package shared;
+
+public class ProtocolStrings {
+  public static final String STOP = "##STOP##";
+  public static final String LOGIN = "LOGIN";
+  public static final String MSG = "CLIENTLIST";
+  public static final String LOGOUT = "LOGOUT";
+  public static final String CLIENTLIST = "MSG";
+  public static final String MSGRES = "LOGIN";
+  
+}
