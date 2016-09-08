a7fc6c74980c7683c5f00a8dfe77cee3182a10cd
rezalt <rezalt@rezalt-Latitude-E6400>
2016-09-08 14:57:47 +0200

    Moved methods to ProtocolString to clean up the ClientHandler.

--- src/main/java/com/mycompany/ca1/server/ClientHandler.java
+++ src/main/java/com/mycompany/ca1/server/ClientHandler.java
@@ -8,10 +8,7 @@ package com.mycompany.ca1.server;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Random;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,88 +18,45 @@ import shared.ProtocolStrings;
  *
  * @author TimmosQuadros
  */
-public class ClientHandler extends Thread {
+public class ClientHandler extends Thread
+{
 
     private Socket socket;
     private Scanner input;
     private PrintWriter writer;
-    private String Username;
-    //private List<ClientHandler> clients = new ArrayList<>();
+    private String username;
+    private ProtocolStrings protocol;
 
-    public Socket getSocket() {
+    public Socket getSocket()
+    {
         return socket;
     }
 
-    public ClientHandler(Socket socket) throws IOException {
+    public ClientHandler(Socket socket) throws IOException
+    {
         this.socket = socket;
         input = new Scanner(socket.getInputStream());
         writer = new PrintWriter(socket.getOutputStream(), true);
         //this.clients = EchoServer.clients;
-        this.Username = "";
+        this.username = "";
+        
+        protocol = new ProtocolStrings(socket);
     }
 
     @Override
-    public void run() {
-        try {
-//            Random r = new Random();
-//            int num = r.nextInt(9999) + 1000;
-//            while (true) {
-//                if (num == 3675) {
-//                    break;
-//                }
-//                writer.printf(Integer.toString(num));
-//                num = r.nextInt(9999) + 1000;
-//            }
-//            writer.printf("%n");
-//            writer.printf("Velkommen til matrix%n");
-            //System.out.println(String.format("Received the message: %1$S ", message));
-            String message = input.nextLine(); //IMPORTANT blocking call
-            while (Username.equalsIgnoreCase("")) {
-                String msg[] = message.split(":");
-                if (msg[0].equalsIgnoreCase("LOGIN") && msg.length > 1 && !usernameExists(msg[1])) {
-                    Username = msg[1];
-                    writer.println("Hello " + Username);
-                    EchoServer.clients.add(this);
-                    sendClientList(); //Send the updated clientlist to all clients
-                }
-                message = input.nextLine(); //IMPORTANT blocking call
-            }
+    public void run()
+    {
+        try
+        {
 
-            Logger.getLogger(Log.LOG_NAME).log(Level.INFO, String.format("Received the message: %1$S ", message));
-//            message = input.nextLine(); //IMPORTANT blocking call
-            while (!message.equals(ProtocolStrings.ARGS.LOGOUT + ":")) {
-                String msg[] = message.split(":");
-                String[] names;
-                if (msg[0].equalsIgnoreCase("LOGIN")) {
-                    writer.println("Already Logged in");
-                } else if (msg[0].equalsIgnoreCase("MSG")) {
-                    if (msg[1].equals("")) {
-                        names = new String[EchoServer.clients.size()];
-                        for (int i = 0; i < EchoServer.clients.size(); i++) {
-                            if (!EchoServer.clients.get(i).getUsername().equals(Username)) {
-                                names[i] = EchoServer.clients.get(i).getUsername();
-                            }
-                        }
-                    } else {
-                        names = msg[1].split(",");
-                        if (names.length == 0) {
-                            String[] tmp = {msg[1]};
-                            names = tmp;
-                        }
-                    }
-                    for (String name : names) {
-                        for (ClientHandler client : EchoServer.clients) {
-                            if (client.getUsername().equals(name)) {
-                                client.sendMessage(ProtocolStrings.ARGS.MSGRESP + ":" + Username + ":" + msg[2]);
-                            }
-                        }
-                    }
-                }
-                //writer.println(message.toUpperCase());
-                //System.out.println(String.format("Received the message: %1$S ", message.toUpperCase()));
-                Logger.getLogger(Log.LOG_NAME).log(Level.INFO, String.format("Received the message: %1$S ", message.toUpperCase()));
-                message = input.nextLine(); //IMPORTANT blocking call
-            }
+            username = protocol.login();
+            EchoServer.clients.add(this);
+            sendClientList(); //Send the updated clientlist to all clients
+            System.out.println("ClientList echo: " + EchoServer.clients);
+            protocol.message(getUsername());
+
+            //  message = input.nextLine(); //IMPORTANT blocking call
+
             EchoServer.clients.remove(this);
             sendClientList();
             writer.println(ProtocolStrings.ARGS.LOGOUT);//Echo the stop message back to the client for a nice closedown
@@ -110,47 +64,43 @@ public class ClientHandler extends Thread {
             //System.out.println("Closed a Connection");
             Logger.getLogger(Log.LOG_NAME).log(Level.INFO, "Closed a Connection");
             super.run(); //To change body of generated methods, choose Tools | Templates.
-        } catch (NoSuchElementException | IOException ex) {
+        }
+        catch (NoSuchElementException | IOException ex)
+        {
             //Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public void sendMessage(String message) {
+    public void sendMessage(String message)
+    {
         writer.println(message);
     }
 
-    public void sendClientList() {
-        String CLIENTLIST = ProtocolStrings.ARGS.CLIENTLIST + ":";
-        for (ClientHandler client : EchoServer.clients) {
-            CLIENTLIST += client.getUsername() + ",";
+    public void sendClientList()
+    {
+        String clientList = ProtocolStrings.ARGS.CLIENTLIST + ":";
+        for (ClientHandler client : EchoServer.clients)
+        {
+            clientList += client.getUsername() + ",";
         }
-        CLIENTLIST = CLIENTLIST.substring(0, CLIENTLIST.length() - 1);
-
-        for (ClientHandler client : EchoServer.clients) {
-            client.sendMessage(CLIENTLIST);
-        }
-    }
-
-    private boolean usernameExists(String username) {
-        for (ClientHandler client : EchoServer.clients) {
-            if (username.equalsIgnoreCase(client.getUsername())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void printClients() {
-        for (ClientHandler client : EchoServer.clients) {
-            System.out.println(client.getUsername() + "%n");
+        clientList = clientList.substring(0, clientList.length() - 1);
+        System.out.println("ClientList: " + clientList);
+        for (ClientHandler client : EchoServer.clients)
+        {
+            client.sendMessage(clientList);
         }
     }
 
-    public String getUsername() {
-        return Username;
+    private void printClients()
+    {
+        for (ClientHandler client : EchoServer.clients)
+        {
+            System.out.println(username + "%n");
+        }
     }
 
-    private static void handleClient(Socket socket) throws IOException {
-
+    public String getUsername()
+    {
+        return username;
     }
 }
--- src/main/java/shared/ProtocolStrings.java
+++ src/main/java/shared/ProtocolStrings.java
@@ -1,50 +1,138 @@
 package shared;
+import com.mycompany.ca1.server.ClientHandler;
+import com.mycompany.ca1.server.EchoServer;
+import com.mycompany.ca1.server.Log;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class ProtocolStrings {
+
+public class ProtocolStrings
+{
     
-    public static enum ARGS{
-        STOP,LOGIN,MSG,LOGOUT,CLIENTLIST,MSGRESP;
+    private Socket socket;
+    private PrintWriter writer;
+    private String username = "";
+    private Scanner input; 
+    private String message;
+    
+    public ProtocolStrings(Socket socket) throws IOException
+    {
+        input =  new Scanner(socket.getInputStream());
+        this.socket = socket;
     }
     
-    public static final String STOP = "STOP";
     
-    public void getMethod(String q){
-        if(q.startsWith("MSG:")){
-            message(q);
-        }
-        if(q.startsWith("LOGIN:")){
-            
-        }
-        if(q.startsWith("LOGOUT:")){
-            
-        }
-        if(q.startsWith("MSGRES:")){
-            
-        }
-        if(q.startsWith("CLIENTLIST:")){
-            
-        }
-    }
     
-    public String message(String q){
-        return q.substring(4);
+
+  
+    public static enum ARGS
+    {
+        STOP, LOGIN, MSG, LOGOUT, CLIENTLIST, MSGRESP;
     }
-    public String login(){
-        return "Login Success";
+
+    public final String STOP = "STOP";
+
+    public void message(String username)
+    {  
+        message = input.nextLine(); //IMPORTANT blocking call
+        while (!message.equals(ProtocolStrings.ARGS.LOGOUT + ":")) {
+                String msg[] = message.split(":");
+                String[] names;
+                if (msg[0].equalsIgnoreCase("LOGIN")) {
+               //    writer.println("MSGRES:SERVER:Already Logged in");
+                } else if (msg[0].equalsIgnoreCase("MSG")) {
+                    if (msg[1].equals("")) {
+                        names = new String[EchoServer.clients.size()];
+                        for (int i = 0; i < EchoServer.clients.size(); i++) {
+                            if (!EchoServer.clients.get(i).getUsername().equals(username)) {
+                                names[i] = EchoServer.clients.get(i).getUsername();
+                            }
+                        }
+                    } else {
+                        names = msg[1].split(",");
+                        if (names.length == 0) {
+                            String[] tmp = {msg[1]};
+                            names = tmp;
+                        }
+                    }
+                    for (String name : names) {
+                        for (ClientHandler client : EchoServer.clients) {
+                            if (client.getUsername().equals(name)) {
+                                client.sendMessage(ProtocolStrings.ARGS.MSGRESP + ":" + username + ":" + msg[2]);
+                            }
+                        }
+                    }
+                }
+                //writer.println(message.toUpperCase());
+                //System.out.println(String.format("Received the message: %1$S ", message.toUpperCase()));
+                Logger.getLogger(Log.LOG_NAME).log(Level.INFO, String.format("Received the message: %1$S ", message.toUpperCase()));
+                message = input.nextLine(); //IMPORTANT blocking call
+            }
     }
-    public String logout(){
+
+    public String login()
+    {
+
+
+        while (username.equalsIgnoreCase(""))
+        {
+            message = input.nextLine(); //IMPORTANT blocking call
+            String msg[] = message.split(":");
+            if (msg[0].equalsIgnoreCase("LOGIN") && msg.length > 1)
+            {
+                username = msg[1];  
+            }
+           
+        }
+
+        return username;
+    }
+
+    public String logout()
+    {
         return "Logged out!";
     }
-    public String clientList(){
+
+    public String clientList()
+    {
         return "";
     }
-    public String msgRes(){
+
+    public String msgRes()
+    {
         return "";
     }
-    public String sendMessage(ARGS args, String msg){
-        if(args.equals(ARGS.MSGRESP)){
-            
+
+    public String sendMessage(ARGS args, String msg)
+    {
+        if (args.equals(ARGS.MSGRESP))
+        {
+
         }
-        return args+":"+msg;
+        return args + ":" + msg;
     }
+
+    public void sendMessage(String message)
+    {
+        System.out.println(""+message);
+        writer.println(message);
+    }
+
+  
+    private boolean usernameExists(String username)
+    {
+        for (ClientHandler client : EchoServer.clients)
+        {
+            if (username.equalsIgnoreCase(username))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
